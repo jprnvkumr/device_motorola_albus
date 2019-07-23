d9e2e3ca72bb764b71d2a62fe1bad1336b61c6c1
jprnvkumr <jprnvkumr@gmail.com>
2019-07-23 13:22:11 +0530

    albus : update libshims.mk

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -40,17 +40,12 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libqsap_shim.c
 LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog libcutils libutils
 LOCAL_C_INCLUDES := $(DEVICE_PATH)/softap/sdk
-LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
 LOCAL_MODULE := libqsap_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := ASensorManager.cpp
-LOCAL_SHARED_LIBRARIES := android.hardware.sensors@1.0
-LOCAL_STATIC_LIBRARIES := android.hardware.sensors@1.0-convert
-LOCAL_MODULE := libshim_ril
 LOCAL_SRC_FILES := justshoot_shim.cpp
 LOCAL_SHARED_LIBRARIES := libutils
 LOCAL_MODULE := libjustshoot_shim
@@ -62,17 +57,9 @@ include $(BUILD_SHARED_LIBRARY)
 # Camera
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libgpu_mapper_shim.cpp
-LOCAL_SRC_FILES := gpu_mapper_shim.cpp
 LOCAL_SHARED_LIBRARIES := libgui libutils
 LOCAL_MODULE := libgpu_mapper_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := justshoot_shim.cpp
-LOCAL_SHARED_LIBRARIES := libutils
-LOCAL_MODULE := libjustshoot_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
 
